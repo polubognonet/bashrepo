0673ada37fa925da2f702cc9deef77496b4571b3
polubognonet <nfyz228@gmail.com>
2021-07-19 21:51:15 +0300

    commited information

---
+++ firstscript.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+count=$1
+echo "$count"
+
+if [[ $count -eq 10 ]]; then
+   echo "This is true, it's 10"
+elif [[ $count -eq 9 ]]; then
+  echo "it is not, it is 9"
+else
+  echo "it is some different number"
+fi
+
+
+
+while [[ $count -le 10 ]]; do
+  count=$(( count+1 ))
+  echo "$count"
+done
+
+for (( count = 0; count < 10; count++ )); do
+  if [[ $count -gt 3 ]] && [[ $count -lt 7 ]];
+  then
+    echo "$count"
+  fi
+done
+
+args=("$0")
+echo $(args[0]) $(args[1]) $(args[2])
